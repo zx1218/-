b1185ad75563d77e52e504dc2413f9e2eb14df20
dc007 <dc007@zhangxuan.local>
2016-06-16 09:44:13 +0800

    Initial Commit

---
+++ 0616--约束/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  0616--约束
+//
+//  Created by dc007 on 16/6/16.
+//  Copyright © 2016年 NSObject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 0616--约束/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  0616--约束
+//
+//  Created by dc007 on 16/6/16.
+//  Copyright © 2016年 NSObject. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
